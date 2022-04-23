7e0281648d58a89ad46a075d841c8225edbe3746
IlNat <ilanatalcenko18@gmail.com>
2022-04-23 17:44:27 +0500

    Add code.

---
+++ NatIlya1104HW.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+char user_field[10][10];
+
+void print() {
+	
+	ifstream user_field_input("UBF.txt");
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			user_field_input >> user_field[i][j];
+			cout << user_field[i][j];
+		}
+		cout << '\n';
+	}
+}
+
+
+
+
+int main()
+{
+    print();
+}
